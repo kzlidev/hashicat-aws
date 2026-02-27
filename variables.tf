3b1f9c6751e779bcce5f27194f0f24f310803fbd
KZ Li <156631869+kzlidev@users.noreply.github.com>
2026-02-27 11:20:15 +0800

    Removed default variables

--- variables.tf
+++ variables.tf
@@ -11,40 +11,40 @@ variable "prefix" {
 
 variable "region" {
   description = "The region where the resources are created."
-  default     = "ap-southeast-1"
+  # default     = "ap-southeast-1"
 }
 
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
+  # default     = "10.0.0.0/16"
 }
 
 variable "subnet_prefix" {
   description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
+  # default     = "10.0.10.0/24"
 }
 
 variable "instance_type" {
   description = "Specifies the AWS instance type."
-  default     = "t2.large"
+  # default     = "t2.large"
 }
 
 variable "admin_username" {
   description = "Administrator user name for mysql"
-  default     = "hashicorp"
+  # default     = "hashicorp"
 }
 
 variable "height" {
-  default     = "800"
+  # default     = "800"
   description = "Image height in pixels."
 }
 
 variable "width" {
-  default     = "800"
+  # default     = "800"
   description = "Image width in pixels."
 }
 
 variable "placeholder" {
-  default     = "placebear.com"
+  # default     = "placebear.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
